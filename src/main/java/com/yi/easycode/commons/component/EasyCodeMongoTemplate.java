aaef6dc2a67e85b38bc4f847fbd983fc065e2254
易志成 <yizhicheng@wings-tech.cn>
2020-12-27 21:28:32 +0800

    easy-code加入Mongodb配置

---
+++ src/main/java/com/yi/easycode/commons/component/EasyCodeMongoTemplate.java
@@ -0,0 +1,66 @@
+package com.yi.easycode.commons.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yizhicheng
+ * @ClassName EasyCodeMongoTemplate
+ * @Description MongoDB 模板类
+ * @Date 2020/12/27 9:03 下午
+ **/
+@Component
+@Slf4j
+public class EasyCodeMongoTemplate {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * 新增
+     * @param obj
+     * @param <T>
+     * @return
+     */
+    public  <T> T save(T obj) {
+        log.info("save mongodb ,{}",obj);
+        return mongoTemplate.save(obj);
+    }
+
+    /**
+     * 查询多个
+     * @param param
+     * @param paramVal
+     * @param entityClass
+     * @param <T>
+     * @return
+     */
+    public  <T> T find(String param,Object paramVal,Class entityClass) {
+        log.info("find mongodb,param:{},paramVal:{}",param,paramVal);
+        Query query = new Query();
+        query.addCriteria(Criteria.where(param).is(paramVal));
+        return (T) mongoTemplate.find(query,entityClass);
+    }
+
+    /**
+     * 查询单个
+     * @param param
+     * @param paramVal
+     * @param entityClass
+     * @param <T>
+     * @return
+     */
+    public  <T> T findOne(String param,Object paramVal,Class entityClass) {
+        log.info("findOne mongodb,param:{},paramVal:{}",param,paramVal);
+        Query query = new Query();
+        query.addCriteria(Criteria.where(param).is(paramVal));
+        return (T) mongoTemplate.findOne(query,entityClass);
+    }
+
+
+
+}
---
+++ src/test/java/com/yi/easycode/DBInfoTest.java
@@ -0,0 +1,42 @@
+package com.yi.easycode;
+
+import com.yi.easycode.modules.entity.mongodb.DBInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author yizhicheng
+ * @ClassName DBInfoTest
+ * @Description 测试存放dbinfo
+ * @Date 2020/12/27 8:26 下午
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@PropertySource(value = "classpath:/mysql-generator.properties")
+public class DBInfoTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+    @Test
+    public void save() {
+        DBInfo dbInfo = new DBInfo();
+        dbInfo.setUserId(3L);
+        dbInfo.setConnectionName("连接1");
+        dbInfo.setType("MYSQL");
+        dbInfo.setUrl("127.0.0.1");
+        dbInfo.setPort("3306");
+        dbInfo.setName("root");
+        dbInfo.setPassword("passw0rd");
+        dbInfo.setDatabaseName("easy-code");
+        mongoTemplate.save(dbInfo);
+
+    }
+
+}
